04695727d66884ef25c5235580e6af5b9bb9cf55
mdsnz <maedson001001@gmail.com>
2025-06-23 17:17:52 -0300

    Finish the whole thing

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let computerSelection;
 function playGame() {
   renderChoices();
 
-  const rockBtn = document.querySelector('.rockBtn');
-  const paperBtn = document.querySelector('.paperBtn');
-  const scissorBtn = document.querySelector('.scissorBtn');
+  const rockBtn = document.querySelector(".rockBtn");
+  const paperBtn = document.querySelector(".paperBtn");
+  const scissorBtn = document.querySelector(".scissorBtn");
 
   let humanSelection;
 
@@ -26,9 +26,7 @@ function playGame() {
     computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection, humanScore, computerScore);
   });
-
 }
-
 
 function getComputerChoice() {
   let computerChoice;
@@ -45,53 +43,64 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+const choiceContent = document.createElement("div");
+const winOrLose = document.createElement("div");
+
 function playRound(humanChoice, computerChoice) {
-    
   if (
     (humanChoice === "ROCK") & (computerChoice === "SCISSOR") ||
     (humanChoice === "SCISSOR") & (computerChoice === "PAPER") ||
     (humanChoice === "PAPER") & (computerChoice === "ROCK")
   ) {
     humanScore++;
-    console.log(
-      `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`
-    );
-    console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`)
+    choiceContent.innerHTML = ``;
+    choiceContent.innerHTML = `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`;
+    renderPlacar();
+    gameArea.appendChild(choiceContent);
   } else if (
     (humanChoice === "ROCK") & (computerChoice === "PAPER") ||
     (humanChoice === "SCISSOR") & (computerChoice === "ROCK") ||
     (humanChoice === "PAPER") & (computerChoice === "SCISSOR")
   ) {
     computerScore++;
-    console.log(
-      `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`
-    );
-        console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`)
+    choiceContent.innerHTML = ``;
+    choiceContent.innerHTML = `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`;
+    renderPlacar();
+    gameArea.appendChild(choiceContent);
   } else if (
     (humanChoice === "ROCK") & (computerChoice === "ROCK") ||
     (humanChoice === "PAPER") & (computerChoice === "PAPER") ||
     (humanChoice === "SCISSOR") & (computerChoice === "SCISSOR")
   ) {
-    console.log(
-      `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`
-    );
-        console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore}`)
+    choiceContent.innerHTML = ``;
+    choiceContent.innerHTML = `Your choice: ${humanChoice} | Computer choice: ${computerChoice}`;
+    renderPlacar();
+    gameArea.appendChild(choiceContent);
   }
 
   if (humanScore === 5) {
-    console.log(`Você ganhou! Parabéns`)
+    choiceContent.innerHTML = ``;
+    gameArea.appendChild(choiceContent);
+    winOrLose.innerHTML = `Você ganhou! Parabéns`;
+    gameArea.appendChild(winOrLose);
     humanScore = 0;
     computerScore = 0;
   } else if (computerScore === 5) {
-    console.log(`Você perdeu para o computador! Patético.`)
+    choiceContent.innerHTML = ``;
+    gameArea.appendChild(choiceContent);
+    winOrLose.innerHTML = `Você perdeu para o computador! Patético.`;
+    gameArea.appendChild(winOrLose);
     humanScore = 0;
     computerScore = 0;
+  } else {
+    winOrLose.innerHTML = ``;
+    gameArea.appendChild(winOrLose);
   }
 }
 
-function renderChoices() {
-  const gameArea = document.querySelector(".gameArea");
+const gameArea = document.querySelector(".gameArea");
 
+function renderChoices() {
   const choices = document.createElement("div");
 
   choices.innerHTML = `
@@ -101,19 +110,28 @@ function renderChoices() {
   <button class="rockBtn">Rock</button>
   <button class="paperBtn">Paper</button>
   <button class="scissorBtn">Scissor</button>
+
   `;
 
   gameArea.appendChild(choices);
 }
 
+let alreadyRender = false;
+const placar = document.createElement("Div");
 
+function renderPlacar() {
+  if (alreadyRender) {
+    placar.innerHTML = ``;
+  }
 
-/*
-1. Clica Start Game
-2. Botão start game some
-3. Aparece: Placar atual e Três botões para seleção 
-4. Após a última seleção, três botões somem, aparece botão jogar novamente, placar final com resultado
-*/
+  placar.innerHTML = `
+    <div>
+    Placar!! Você: ${humanScore} || Computador: ${computerScore}
+    </div>
+    `;
+  gameArea.appendChild(placar);
+  alreadyRender = true;
+}
 
 // selecionando botão start
 const startBtn = document.querySelector(".startBtn");
